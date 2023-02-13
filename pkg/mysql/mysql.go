6a731da17299578e4b68288f36bedca797296668
Darwis <107021419+darwisrh@users.noreply.github.com>
2023-02-13 09:43:16 +0700

    result

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func DatabaseInit() {
 	var err error
-	dsn := "root:V65YOKD4PkKa470sFEqm@tcp(containers-us-west-72.railway.app:7804)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:BxWcBuXpW8GlnGW8SIrm@tcp(containers-us-west-78.railway.app:7007)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
